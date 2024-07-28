22fdce4494284c491ae6a5dc35658a73e601cb59
ssol-park <ksttlov.naver@hanmail.net>
2024-07-28 21:42:04 +0900

    chore: add initial project configuration files

---
+++ src/main/java/com/keywith/api/KeywithApplication.java
@@ -0,0 +1,13 @@
+package com.keywith.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeywithApplication.class, args);
+    }
+
+}
