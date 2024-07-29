5ff2768e3d27092c253bf50179de4f8ca611696e
ssol-park <ksttlov.naver@hanmail.net>
2024-07-29 22:25:54 +0900

    feat: Create ScrapProperties
    - Added scraping settings to application.yml

---
+++ src/main/java/com/keywith/api/ScrapProperties.java
@@ -0,0 +1,15 @@
+package com.keywith.api;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "scrap")
+public class ScrapProperties {
+    private String domain;
+    private String baseUrl;
+}
