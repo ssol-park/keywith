975811cc09b0caab9a970c3aad5aef301759314b
ssol-park <ksttlov.naver@hanmail.net>
2024-08-24 18:22:05 +0900

    feat: Add setter to ScrapProperties

--- src/main/java/com/keywith/api/config/ScrapProperties.java
+++ src/main/java/com/keywith/api/config/ScrapProperties.java
@@ -1,10 +1,12 @@
 package com.keywith.api.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "scrap")
 public class ScrapProperties {
